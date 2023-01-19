378a7ea348e383fd190a488471625c8437a73dfe
JoãoPedro <106089677+JaoPedro007@users.noreply.github.com>
2023-01-19 17:59:43 -0300

    Company Search with mask and button for Unlock

--- companySearch/src/App.jsx
+++ companySearch/src/App.jsx
@@ -1,7 +1,8 @@
 import { useState } from 'react'
 import axios from 'axios'
 import moment from 'moment'
-import {ToastContainer, toast} from 'react-toastify';
+import { ToastContainer, toast } from 'react-toastify';
+import { IMaskInput } from 'react-imask';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -9,42 +10,55 @@ function App() {
   const [cnpj, setCNPJ] = useState();
   const [lock_date, setLockDate] = useState();
   const [phone, setPhone] = useState();
+  const [is_blocked, setIsBlocked] = useState();
 
-
-
-  async function companySearch() {
-    let api = 'https://api.gtech.site/companies/:cnpj/is_blocked'
-    api = api.replace(':cnpj', cnpj);
-
-
-
-      await axios.get(api).then(e => {
-        if (e.data != null) {
-          console.log(`''${JSON.stringify(e.data)}''`, 'asdasdasdad')
-          if(!JSON.stringify(e.data).includes('""')){
-            setEmpresa(e.data)
-
-          }else{
-            console.log('Cannot to Search the Company');
-            toast.error('ERROR')
-        
-          }
-        }
-
-      }).catch(e => {
-        console.log(e);
-      })
-      
-    }
-
+  let api = 'https://api.gtech.site/companies/:cnpj/is_blocked'
 
   
 
 
 
+  async function companySearch() {
+    var cnpjFormated = cnpj.replace(/[^0-9]/g, '');
+    api = api.replace(':cnpj', cnpjFormated);
+
+    await axios.get(api).then(e => {
+      if (e.data != null) {
+
+        console.log(`${JSON.stringify(e.data)}`)
+        if (!JSON.stringify(e.data).includes('""')) {
+          setEmpresa(e.data)
+
+        } else {
+          toast.error('Cannot to Search the Company')
+        }
+      }
+
+    })
+
+  }
+  async function companyUnlock(){
+    var cnpjFormated = cnpj.replace(/[^0-9]/g, '');
+    api = api.replace(':cnpj', cnpjFormated);
+
+
+    axios.post(api, {
+      is_blocked: true
+     
+    })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
+
+
+
   function KeyDownEnter(e) {
     if (e.code === 'Enter' || e.code === 'NumpadEnter') {
-      companyVerify()
+      companySearch()
     }
 
   }
@@ -54,53 +68,78 @@ function App() {
 
   return (
 
-    <div className='body'>
-      <ToastContainer 
-theme="dark"/>
 
-      <div className='flex flex-col bg-indigo-900 w-full h-screen items-center'>
+    <div className='body'>
+      <ToastContainer
+        theme="light" />
+
+
+      <div className='flex flex-col bg-gray-800 w-full h-screen items-center'>
 
         <div className="mx-auto max-w-screen-xl px-5 py-16 sm:px-6 lg:px-8">
           <div className="mx-auto max-w-lg text-center">
-            <h1 className="text-2xl text-gray-200 font-bold sm:text-3xl">Company Search!</h1>
+            <h1 className="text-2xl text-gray-200 font-bold sm:text-4xl">Company Search!</h1>
 
-            <p className="mt-4 text-gray-300">
+            <p className="mt-3 text-gray-300 sm:text-1xl">
               Type a CNPJ and click on button Search to show details
             </p>
           </div>
 
-          <div className="mx-auto mt-8 mb-0 max-w-md space-y-4">
+          <div className="mx-auto mt- mb-0 max-w-md space-y-4">
             <div>
 
               <div className="relative">
-                <input
+                <IMaskInput mask="00.000.000/0000-00"
                   placeholder='Enter the CNPJ'
                   value={cnpj} onChange={e => setCNPJ(e.target.value)} onKeyDown={KeyDownEnter}
-                  type="number"
                   className="w-full rounded-lg border-gray-200 p-4 text-sm shadow-sm"
                 />
+
+              </div>
+
+
+            </div>
+            <div className='flex justify-between'>
+
+              <div onClick={companyUnlock}>
+                <button>
+                  <p></p>
+                  <a
+                    className="group relative inline-block overflow-hidden border border-black px-8 py-3 focus:outline-none focus:ring"
+                  >
+                    <span
+                      className="absolute inset-y-0 left-0 w-[2px] bg-black transition-all group-hover:w-full group-active:bg-black">
+                    </span>
+
+                    <span
+                      className="relative text-sm font-medium text-gray-400 transition-colors group-hover:text-white" >
+                      Unlock
+                    </span>
+
+                  </a>
+                </button>
+              </div>
+
+              <div onClick={companySearch}>
+
+                <button>
+                  <a
+                    className="group relative inline-block overflow-hidden border border-black px-8 py-3 focus:outline-none focus:ring"
+
+                  >
+                    <span
+                      className="absolute inset-y-0 right-0 w-[2px] bg-black transition-all group-hover:w-full group-active:bg-black"
+                    ></span>
+
+                    <span
+                      className="relative text-sm font-medium text-gray-400 transition-colors group-hover:text-white"
+                    >
+                      Search
+                    </span>
+                  </a>
+                </button>
               </div>
             </div>
-
-            <div className='flex flex-row justify-end' onClick={companySearch}>
-
-              <button className=" flex items-center justify-between">
-                <p></p>
-                <a
-                  className="group relative inline-block overflow-hidden border border-indigo-600 px-8 py-3 focus:outline-none focus:ring"
-                >
-                  <span
-                    className="absolute inset-y-0 left-0 w-[2px] bg-indigo-600 transition-all group-hover:w-full group-active:bg-indigo-500">
-                  </span>
-
-                  <span
-                    className="relative text-sm font-medium text-indigo-600 transition-colors group-hover:text-white" >
-                    Search
-                  </span>
-                </a>
-              </button>
-            </div>
-
           </div>
         </div>
 
